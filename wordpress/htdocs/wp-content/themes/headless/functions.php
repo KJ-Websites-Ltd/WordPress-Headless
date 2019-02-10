e5506adb3dd58d2000eadb1efb240bce5b51240b
John Jeeves <john@kjwebsites.co.uk>
2019-02-10 18:51:32 +0000

    setup simple theme ith headless settings

--- wordpress/htdocs/wp-content/themes/headless/functions.php
+++ wordpress/htdocs/wp-content/themes/headless/functions.php
@@ -44,6 +44,10 @@ add_filter( 'wp_headless_rest__enable_jwt', '__return_false' );
 
 
 
+
+
+
+
 if ( ! function_exists( 'headless_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
